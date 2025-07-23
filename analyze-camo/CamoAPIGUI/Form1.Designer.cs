db9db14fc42e549b30c5e52d22c1e428857e277f
MillerPatrick214 <149214352+MillerPatrick214@users.noreply.github.com>
2025-07-23 14:55:45 -0700

    Some results...
    Some results of mixed accuracy. I think I will be moving away from k clustering entirely for the analyze-camo portion of this project. I'm creating a quick/simple C# gui that will provide much better accuracy.

---
+++ analyze-camo/CamoAPIGUI/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace CamoAPIGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            trackBar1 = new TrackBar();
+            ColorAdd = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            listBox1 = new ListBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            colorDialog1 = new ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(747, 751);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(1201, 27);
+            trackBar1.Maximum = 100;
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(147, 45);
+            trackBar1.TabIndex = 1;
+            trackBar1.Scroll += trackBar1_Scroll;
+            // 
+            // ColorAdd
+            // 
+            ColorAdd.Location = new Point(765, 489);
+            ColorAdd.Name = "ColorAdd";
+            ColorAdd.Size = new Size(75, 54);
+            ColorAdd.TabIndex = 3;
+            ColorAdd.Text = "Add Entry";
+            ColorAdd.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(1273, 489);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 54);
+            button2.TabIndex = 4;
+            button2.Text = "Finish";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(1168, 489);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 54);
+            button3.TabIndex = 5;
+            button3.Text = "Delete";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(1273, 740);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 6;
+            button4.Text = "Clear All";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(765, 549);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(583, 184);
+            listBox1.TabIndex = 7;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(783, 209);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(120, 119);
+            pictureBox2.TabIndex = 8;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(783, 27);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(120, 119);
+            pictureBox3.TabIndex = 9;
+            pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(783, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 15);
+            label1.TabIndex = 10;
+            label1.Text = "Selected Area";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(783, 191);
+            label2.Name = "label2";
+            label2.Size = new Size(120, 15);
+            label2.TabIndex = 11;
+            label2.Text = "Selection Mean Color";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(846, 489);
+            button1.Name = "button1";
+            button1.Size = new Size(132, 54);
+            button1.TabIndex = 12;
+            button1.Text = "Apply Mean Color To Entry";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(1249, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 15);
+            label3.TabIndex = 0;
+            label3.Text = "Area size";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(1279, 416);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 13;
+            label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1360, 775);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(listBox1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(ColorAdd);
+            Controls.Add(trackBar1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;     // Display area f
+        private TrackBar trackBar1;
+        private Button ColorAdd;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private ListBox listBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Label label3;
+        private Label label4;
+        private ColorDialog colorDialog1;
+    }
+}
